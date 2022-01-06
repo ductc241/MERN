3bac5aaa0ba1226e762539638a24de1b6ef44f98
ductc241 <tacongduc123@gmail.com>
2022-01-06 21:30:08 +0700

    Backend - User API - completed

--- controllers/use.controller.js
+++ controllers/use.controller.js
@@ -45,9 +45,65 @@ const userController = {
             res.json({ accessToken })
 
         } catch (error) {
-            res.status(500).json({
+            return res.status(500).json({
                 msg: error.message
             })
+        }
+    },
+
+    login: async (req, res) => {
+        try {
+            const { email, password} = req.body
+
+            // check user infor
+            const user = await Users.findOne({ email })
+            if(!user) return res.status(400).json({ msg: 'Cant find user' })
+
+            const isMatch = await bcrypt.compare(password, user.password)
+            if(!isMatch) return res.status(400).json({ msg: 'Incorrect password' })
+
+            // Response
+            const accessToken =  jwt.sign(
+                {id: user._id},
+                process.env.ACCESS_TOKEN_SECRET,
+                { expiresIn: '1d' }
+            )
+
+            const refreshToken = jwt.sign(
+                {id: user._id},
+                process.env.REFRESH_TOKEN_SECRET,
+                { expiresIn: '2d' }
+            )
+
+            res.cookie('refreshtoken', refreshToken, {
+                httpOnly: true,
+                path: '/user/refresh_token'
+            })
+
+            res.json({ accessToken })
+            
+        } catch (error) {
+            return res.status(500).json({ msg: error.message })
+        }
+    },
+
+    logout: async (req, res) => {
+        try {
+            res.clearCookie('refreshtoken', {path: '/user/refresh_token'})
+            return res.json({ msg: 'Logged out' })
+        } catch (error) {
+            return res.status(500).json({ msg: error.message })
+        }
+    },
+
+    getUser: async (req, res) => {
+        try {
+            const user = await Users.findOne({ id: req.user.id}).select('-password')
+            if(!user) return res.status(400).json({ msg: 'User doesnt find' })
+    
+            res.json(user)     
+        } catch (error) {
+            return res.status(500).json({ msg: error.message })
         }
     }
 }
--- routes/user.router.js
+++ routes/user.router.js
@@ -1,7 +1,11 @@
-const userController = require("../controllers/use.controller");
 const router = require("express").Router();
+const userController = require("../controllers/use.controller");
+const authMiddleware = require("../middleware/auth");
 
 
 router.post('/register', userController.register)
+router.post('/login', userController.login)
+router.post('/logout', userController.logout)
+router.get('/infor', authMiddleware, userController.getUser)
 
 module.exports = router
